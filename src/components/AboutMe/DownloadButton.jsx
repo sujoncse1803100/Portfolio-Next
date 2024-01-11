50699203977ff7614e8165556b4a911c7fb82d28
sujonali8276 <sujonali8276@gmail.com>
2024-01-11 20:07:02 +0600

    resume updated

--- src/components/AboutMe/DownloadButton.jsx
+++ src/components/AboutMe/DownloadButton.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 const DownloadButton = () => {
   return (
     <button className="bg-blue-500 ps-2 pr-2 rounded">
-      <a href="/Sujon_Ali_(Resume).pdf" download="Sujon_Ali_Resume.pdf">
+      <a href="/Sujon_Ali_1.3.pdf" download="Sujon_Ali_Resume.pdf">
         Download CV
       </a>
     </button>
